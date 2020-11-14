114cdbcb6240075404407be8eaa0f6873d585229
Gleb <gslepenkov@gmail.com>
2020-11-14 20:18:58 +0300

    New Version

--- server/app.js
+++ server/app.js
@@ -6,18 +6,21 @@ const modelUrl = 'someurl';
 
 const PORT = process.env.PORT || 4001;
 
+app.use(express.static(path.join(__dirname, 'build')));
+app.use(cors());
 app.get('/', (req, res) => {
     const options = {
         root: path.join(__dirname, 'build'),
         headers: {
-            'Content-Security-Policy': "script-src 'self' http://localhos:4001 'nonce-Nc3n83cnSAd3wc3Sasdfn939hc3'"
+            'Content-Security-Policy': "script-src 'self' http://localhost:4001 'nonce-ZmI5MzlhYzAxNDNiOTk5YWJiNWEzMjE2YmRiZDU2NzA2NjhiNjE5MDhlNWI0N2FjYTJkMTJmODhhNWMxOWJhZA=='"
             //"script-src 'sha256-ZmI5MzlhYzAxNDNiOTk5YWJiNWEzMjE2YmRiZDU2NzA2NjhiNjE5MDhlNWI0N2FjYTJkMTJmODhhNWMxOWJhZA==' "
         }
     }
     res.sendFile('index.html',options);
 });
 
-app.post('/model', (req, res) => {
+app.get('/model', (req, res) => {
+    console.log('it is working')
     res.redirect(modelUrl);
 })
 
